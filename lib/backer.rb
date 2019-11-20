dec854d929b60d86d3801cdd159e468b3f915e70
Michael Calkins <m.calkins576@gmail.com>
2019-11-20 15:58:37 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,21 @@
 class Backer
   attr_reader :name, :backed_projects
   
+  @@all = []
+  
   def initialize(name)
     @backed_projects = []
     @name = name
+    @@all << self
   end
   
   def back_project(project_obj)
     @backed_projects << project_obj
   end
+  
+  def self.all
+    @@all
+  end
+  
   
 end
